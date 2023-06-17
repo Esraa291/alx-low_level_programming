90d309467cd5ae258d0089fa360d7401d4150648
Esraa <esraa.elnagar291@gmail.com>
2023-06-17 14:22:40 -0700

    sec

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -5,8 +5,11 @@
 /**
  * main - Entry point
  *
+ * Description: print the value of n status:
+ *		greater than, is zero and is not less than 6.
+ *
  * Return: Always 0 (Success)
- */
+*/
 int main(void)
 {
 	int n, last_digit;
