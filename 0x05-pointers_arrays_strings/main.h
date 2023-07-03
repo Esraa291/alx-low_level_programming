508f7134a982544c167657a3683567cf9fccc60d
Esraa <esraa.elnagar291@gmail.com>
2023-07-02 18:35:44 -0700

    one

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+/* function prototypes */
+void reset_to_98(int *n);
+int _isdigit(int c);
+int _putchar(char c);
+
+/* macros */
+#define BUFFER_SIZE 1024
+
+#endif /* MAIN_H */
