3a38c436666a6226d37cae0dd79c478b202e7968
Esraa <esraa.elnagar291@gmail.com>
2023-07-16 05:32:19 -0700

    0

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -8,10 +8,10 @@
  */
 void *malloc_checked(unsigned int b)
 {
-	 char *p;
+	char *p;
 
-	  p = malloc(b);
-	  if (p == NULL)
-		  exit(98);
-	   return (p);
+	p = malloc(b);
+	if (p == NULL)
+		exit(98);
+	return (p);
 }
